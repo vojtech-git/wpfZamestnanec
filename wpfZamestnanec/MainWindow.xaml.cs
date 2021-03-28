84c2c68c6ac50e4d81b0ebd7ded5238e2360abae
Vojtěch Pejsar <pejsar.vojtech.2018@skola.ssps.cz>
2021-03-28 21:56:45 +0200

    Nadefinoval jsem základy tříd.

--- wpfZamestnanec/MainWindow.xaml.cs
+++ wpfZamestnanec/MainWindow.xaml.cs
@@ -24,5 +24,39 @@ namespace wpfZamestnanec
         {
             InitializeComponent();
         }
+
+        private void switchFormButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public class Osoba
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int BirthYear { get; set; }
+
+        public Osoba(string _name, string _surname, int _birthYear)
+        {
+            Name = _name;
+            Surname = _surname;
+            BirthYear = _birthYear;
+        }
+    }
+
+    public class Zamestanec : Osoba
+    {
+        public string HighestEducation { get; set; }
+        public string Position { get; set; }
+        public int Salary { get; set; }
+
+        public Zamestanec(string _name, string _surname, int _birthYear, string _highestEducation, string _position, int _salary) : base(_name, _surname, _birthYear)
+        {
+            HighestEducation = _highestEducation;
+            Position = _position;
+            Salary = _salary;
+        }
     }
 }
+
